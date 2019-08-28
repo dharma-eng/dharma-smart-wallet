ce1c848c407d5b003cd0707df1037ea39999916d
0age <0age@protonmail.com>
2019-08-28 10:54:07 -0400

    clean up DharmaSmartWalletFactoryV1

--- contracts/factories/DharmaSmartWalletFactoryV1.sol
+++ contracts/factories/DharmaSmartWalletFactoryV1.sol
@@ -1,54 +1,67 @@
 pragma solidity 0.5.11;
 
-import "../implementations/DharmaSmartWalletImplementationV0.sol";
 import "../UpgradeBeaconProxy.sol";
+
+
+interface DharmaSmartWalletInitializer {
+  function initialize(address dharmaKey) external;
+}
 
 
 /**
  * @title DharmaSmartWalletFactoryV1
  * @author 0age
  * @notice This contract deploys new Dharma smart wallets as upgrade beacon
- * proxies pointing to a shared implementation contract.
+ * proxies pointing to a shared implementation contract specified by the Dharma
+ * Upgrade Beacon contract.
  */
 contract DharmaSmartWalletFactoryV1 {
-  // Fires an event when a new smart wallet is created - this may not be needed.
+  // Fires an event when a new smart wallet is created.
   event SmartWalletDeployed(address wallet, address dharmaKey);
 
-  function newSmartWallet(address dharmaKey) public returns (address wallet) {
+  // Use Dharma Smart Wallet initializer to construct initialization calldata.
+  DharmaSmartWalletInitializer private _initializer;
+
+  /**
+   * @notice Deploy a new smart wallet address using the provided Dharma Key.
+   * @param dharmaKey address The dharma key supplied as a constructor argument.
+   * @return The address of the new smart wallet.
+   */
+  function newSmartWallet(address dharmaKey) external returns (address wallet) {
     // Construct initialization calldata as initialize selector and dharma key.
-    DharmaSmartWalletImplementationV0 implementation;
     bytes memory initializationCalldata = abi.encodeWithSelector(
-      implementation.initialize.selector,
+      _initializer.initialize.selector,
       dharmaKey
     );
     
     // Initialize and deploy user smart wallet as minimal beacon-upgrade proxy.
-    // NOTE: by deploying the dharma smart wallet to a compact address, we can
-    // instead call _spawnCompact (using regular _spawn now for easier testing)
     wallet = _spawn(initializationCalldata);
 
     // Emit an event to signal the creation of a new smart wallet.
     emit SmartWalletDeployed(wallet, dharmaKey);
   }
 
+  /**
+   * @notice View function to find the address of the next smart wallet address
+   * that will be deployed for a given Dharma Key.
+   * @param dharmaKey address The dharma key supplied as a constructor argument.
+   * @return The future address of the next smart wallet.
+   */
   function getNextSmartWallet(
     address dharmaKey
-  ) public view returns (address wallet) {
+  ) external view returns (address wallet) {
     // Construct initialization calldata as initialize selector and dharma key.
-    DharmaSmartWalletImplementationV0 implementation;
     bytes memory initializationCalldata = abi.encodeWithSelector(
-      implementation.initialize.selector,
+      _initializer.initialize.selector,
       dharmaKey
     );
     
     // Determine the user's smart wallet address based on the dharma key.
-    // NOTE: by deploying the dharma smart wallet to a compact address, we can
-    // instead call _computeNextCompactAddress (using now for easier testing)
     wallet = _computeNextAddress(initializationCalldata);
   }
 
   /**
-   * @notice Internal function for spawning a minimal upgradeable proxy using
+   * @notice Private function for spawning a minimal upgradeable proxy using
    * `CREATE2`.
    * @param initializationCalldata bytes The calldata that will be supplied to
    * the `DELEGATECALL` from the spawned contract to the logic contract during
@@ -57,54 +70,17 @@ contract DharmaSmartWalletFactoryV1 {
    */
   function _spawn(
     bytes memory initializationCalldata
-  ) internal returns (address spawnedContract) {
-    // place creation code and constructor args of contract to spawn in memory.
-    bytes memory initCode = abi.encodePacked(
-      type(UpgradeBeaconProxy).creationCode,
-      abi.encode(initializationCalldata)
-    );
-
-    // spawn the contract using `CREATE2`.
-    spawnedContract = _spawnCreate2(initCode);
-  }
-
-  /**
-   * @notice Internal view function for finding the address of the next standard
-   * minimal upgradeable proxy created using `CREATE2` with a given
-   * initialization calldata payload.
-   * @param initializationCalldata bytes The calldata that will be supplied to
-   * the `DELEGATECALL` from the spawned contract to the logic contract during
-   * contract creation.
-   * @return The address of the next spawned minimal upgradeable proxy contract
-   * with the given parameters.
-   */
-  function _computeNextAddress(
-    bytes memory initializationCalldata
-  ) internal view returns (address target) {
-    // place creation code and constructor args of contract to spawn in memory.
-    bytes memory initCode = abi.encodePacked(
-      type(UpgradeBeaconProxy).creationCode,
-      abi.encode(initializationCalldata)
-    );
-
-    // get target address using the constructed initialization code.
-    (, target) = _getSaltAndTarget(initCode);
-  }
-
-  /**
-   * @notice Private function for spawning a minimal upgradeable proxy using
-   * `CREATE2`. Provides logic that is reused by internal functions. A salt will
-   * also be chosen based on the calling address and a computed nonce that
-   * prevents deployments to existing addresses.
-   * @param initCode bytes The contract creation code.
-   * @return The address of the newly-spawned contract.
-   */
-  function _spawnCreate2(
-    bytes memory initCode
   ) private returns (address spawnedContract) {
-    // get salt to use during deployment using the supplied initialization code.
+    // place creation code and constructor args of contract to spawn in memory.
+    bytes memory initCode = abi.encodePacked(
+      type(UpgradeBeaconProxy).creationCode,
+      abi.encode(initializationCalldata)
+    );
+
+    // Get salt to use during deployment using the supplied initialization code.
     (bytes32 salt, ) = _getSaltAndTarget(initCode);
 
+    // Spawn the contract using `CREATE2`.
     assembly {
       let encoded_data := add(0x20, initCode) // load initialization code.
       let encoded_size := mload(initCode)     // load the init code's length.
@@ -115,12 +91,35 @@ contract DharmaSmartWalletFactoryV1 {
         salt                                  // pass in the salt value.
       )
 
-      // pass along failure message from failed contract deployment and revert.
+      // Pass along failure message and revert if contract deployment fails.
       if iszero(spawnedContract) {
         returndatacopy(0, 0, returndatasize)
         revert(0, returndatasize)
       }
     }
+  }
+
+  /**
+   * @notice Private view function for finding the address of the next standard
+   * minimal upgradeable proxy created using `CREATE2` with a given
+   * initialization calldata payload.
+   * @param initializationCalldata bytes The calldata that will be supplied to
+   * the `DELEGATECALL` from the spawned contract to the logic contract during
+   * contract creation.
+   * @return The address of the next spawned minimal upgradeable proxy contract
+   * with the given parameters.
+   */
+  function _computeNextAddress(
+    bytes memory initializationCalldata
+  ) private view returns (address target) {
+    // Place creation code and constructor args of contract to spawn in memory.
+    bytes memory initCode = abi.encodePacked(
+      type(UpgradeBeaconProxy).creationCode,
+      abi.encode(initializationCalldata)
+    );
+
+    // Get target address using the constructed initialization code.
+    (, target) = _getSaltAndTarget(initCode);
   }
 
   /**
@@ -131,20 +130,20 @@ contract DharmaSmartWalletFactoryV1 {
   function _getSaltAndTarget(
     bytes memory initCode
   ) private view returns (bytes32 salt, address target) {
-    // get the keccak256 hash of the init code for address derivation.
+    // Get the keccak256 hash of the init code for address derivation.
     bytes32 initCodeHash = keccak256(initCode);
 
-    // set the initial nonce to be provided when constructing the salt.
+    // Set the initial nonce to be provided when constructing the salt.
     uint256 nonce = 0;
     
-    // declare variable for code size of derived address.
+    // Declare variable for code size of derived address.
     uint256 codeSize;
 
     while (true) {
-      // derive `CREATE2` salt using `msg.sender` and nonce.
+      // Derive `CREATE2` salt using `msg.sender` and nonce.
       salt = keccak256(abi.encodePacked(msg.sender, nonce));
 
-      target = address(    // derive the target deployment address.
+      target = address(            // derive the target deployment address.
         uint160(                   // downcast to match the address type.
           uint256(                 // cast to uint to truncate upper digits.
             keccak256(             // compute CREATE2 hash using 4 inputs.
@@ -159,15 +158,15 @@ contract DharmaSmartWalletFactoryV1 {
         )
       );
 
-      // determine if a contract is already deployed to the target address.
+      // Determine if a contract is already deployed to the target address.
       assembly { codeSize := extcodesize(target) }
 
-      // exit the loop if no contract is deployed to the target address.
+      // Exit the loop if no contract is deployed to the target address.
       if (codeSize == 0) {
         break;
       }
 
-      // otherwise, increment the nonce and derive a new salt.
+      // Otherwise, increment the nonce and derive a new salt.
       nonce++;
     }
   }  
