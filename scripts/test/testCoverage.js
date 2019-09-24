e0598fe80692d9147ea501e65a7e22b3524725cb
0age <0age@protonmail.com>
2019-09-24 14:04:27 -0400

    get coverage working again

--- scripts/test/testCoverage.js
+++ scripts/test/testCoverage.js
@@ -2,10 +2,14 @@ var Web3 = require('web3')
 web3Provider = new Web3('ws://localhost:8555')
 
 // import tests
+var deployMockExternal = require('./deployMockExternal.js')
+var deploy = require('./deploy.js')
 var test = require('./test.js')
 
 // run tests
 async function runTests() {
+  await deployMockExternal.test(web3Provider, 'coverage')
+  await deploy.test(web3Provider, 'coverage')
   await test.test(web3Provider, 'coverage')
   process.exit(0)
 }
