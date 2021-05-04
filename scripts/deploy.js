4affd1419954ffae9668b3c6f25fda749ebb5f81
Carlos Flores <carlos@dharma.io>
2021-05-04 16:45:01 -0700

    implement message signing to set up global key

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,20 @@
 const ethers = require("hardhat").ethers;
+const util = require("ethereumjs-util");
 
-async function getSigner(index = 0) {
-    const signers = await ethers.getSigners();
-    return signers[index];
+async function getSigner() {
+    const mnemonic =
+        "toast input grunt first scare ancient frame canoe flag clinic cereal empty orange hire swap";
+    const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic);
+    return {
+        privateKey: walletMnemonic.privateKey,
+        signer: new ethers.Wallet(walletMnemonic.privateKey, ethers.provider)
+    };
 }
 
 async function signerCallbackDefault(transaction) {
-    const signer = await getSigner();
-    return signer.sendTransaction(transaction);
+    const { signer } = await getSigner();
+    const txn = await signer.sendTransaction(transaction);
+    return txn.wait();
 }
 
 async function deploy(name, args = [], signerCallBack = signerCallbackDefault) {
@@ -17,8 +24,11 @@ async function deploy(name, args = [], signerCallBack = signerCallbackDefault) {
 
     const receipt = await signerCallBack(deployTransaction);
 
-    const contractAddress = receipt.creates || receipt.contractAddres;
+    const contractAddress = receipt.creates || receipt.contractAddress;
 
+    if (!contractAddress) {
+        throw new Error("Could not find contract address");
+    }
     return new ethers.Contract(
         contractAddress,
         contract.interface,
@@ -26,9 +36,51 @@ async function deploy(name, args = [], signerCallBack = signerCallbackDefault) {
     );
 }
 
+async function getKeyRegistrySignature(keyResigtryAddress) {
+    const { privateKey, signer } = await getSigner();
+
+    const hexString =
+        keyResigtryAddress +
+        signer.address.slice(2) +
+        ethers.utils
+            .hexlify(
+                ethers.utils.toUtf8Bytes(
+                    "This signature demonstrates that the supplied signing key is valid."
+                )
+            )
+            .slice(2);
+
+    const sig = util.ecsign(
+        util.toBuffer(
+            ethers.utils.keccak256(
+                // prefix => "\x19Ethereum Signed Message:\n32"
+                "0x19457468657265756d205369676e6564204d6573736167653a0a3332" +
+                    ethers.utils
+                        .keccak256(hexString, { encoding: "hex" })
+                        .slice(2),
+                { encoding: "hex" }
+            )
+        ),
+        util.toBuffer(privateKey)
+    );
+
+    return (
+        util.bufferToHex(sig.r) +
+        util.bufferToHex(sig.s).slice(2) +
+        ethers.utils.hexlify(sig.v).slice(2)
+    );
+}
+
 async function run(signerCallback = signerCallbackDefault) {
     // TODO: use env file when deploying contracts to production environment.
-    const signer = await getSigner();
+    // Set up signer
+    const { signer } = await getSigner();
+    const jsonRPCsigners = await ethers.getSigners();
+    const jsonRPCSigner = jsonRPCsigners[0];
+    await jsonRPCSigner.sendTransaction({
+        to: signer.address,
+        value: ethers.utils.parseEther("100")
+    });
 
     // deploy key-registry
     const dharmaKeyRegistry = await deploy("DharmaKeyRegistryV2");
